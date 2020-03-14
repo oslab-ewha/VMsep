36ed25e8a6c5f2545afcc8345a84c2b48e0ad84a
KyungWoon Cho <cezanne@codemayo.com>
2020-03-14 16:49:31 +0900

    add friendly error messages for usbip unbind
    unbind for a stub device did not provide friendly error messages. This
    lead to unclear diagnosis for unbind failure such as #110. This
    clarifies a little unbind failures.

--- driver/lib/strutil.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ntddk.h>
-
-LPWSTR libdrv_strdupW(LPCWSTR cwstr);
-
-int libdrv_snprintf(char *buf, int size, const char *fmt, ...);
-int libdrv_snprintfW(PWCHAR buf, int size, LPCWSTR fmt, ...);
-int libdrv_asprintfW(PWCHAR *pbuf, LPCWSTR fmt, ...);
